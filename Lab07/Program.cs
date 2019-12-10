2904a1d469571a46f139a00098ab63126890b929
GaryMontgomery <S01143848@acad.tri-c.edu>
2019-12-09 21:42:41 -0500

    lab 7

---
+++ Lab07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab07
+{
+    public class Book
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public int Year { get; }
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+    }
+
+    class Display
+    {
+        static void Main()
+        {
+            var book1 = new Book("A Dance with Dragons", "George R.R. Martin", 2011);
+            Console.WriteLine(book1.Title);
+            Console.WriteLine(book1.Author);
+        }
+    }
+}
